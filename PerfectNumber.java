64b8a96ae200ed05764ae2301cc0de9fa06032b3
Aetheress <55312233+Aetheress@users.noreply.github.com>
2019-09-18 14:10:21 +0300

    Update PerfectNumber.java
    Updated algorithm to implement the squareroot method instead. Essentially the task is done already but ideally I'd like to come up with a way to get rid of that if-else if-else construction until the deadline.

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -12,11 +12,13 @@ public class PerfectNumber {
 	
 	public static Set<Integer> divisors(int n) {
 		Predicate<Integer> divisible = (divisor) -> ((n%divisor)==0);
-		Set<Integer> generator = IntStream.range(1, (n/2)+1).boxed().collect(Collectors.toSet());
+		Set<Integer> generator = IntStream.range(1, (int)Math.round(Math.sqrt(n)+1)).boxed().collect(Collectors.toSet());
 		Set<Integer> divisors = new HashSet<Integer>();
 		generator.stream().filter(divisible).forEach(divisors::add);
-		Consumer<Integer> roleCall = (divisor) -> System.out.println("Divisors of the number " + divisor + " are:");
-		roleCall.accept(n);
+		Function<Integer, Integer> reverseDiv = (divisor) -> n/divisor;
+		divisors.stream().map(reverseDiv).sorted((a,b)->b.compareTo(a)).forEach(divisors::add);
+		divisors.remove(n); //implementing sqrt method allows illegal divisors (n itself)
+		System.out.println("Divisors of the number " + n + " are:");
 		divisors.stream().sorted().forEach(System.out::println);
 		return divisors;
 	}
@@ -25,8 +27,7 @@ public class PerfectNumber {
 				Integer sumReduce = divisors(n).stream().reduce(0, (a,b) -> a+b);
 				Predicate<Integer> deficiency = (sum) -> sum<n;
 				Predicate<Integer> perfection = (sum) -> sum==n;
-				Consumer<Integer> sumAnnounce = (sum) -> System.out.println("Divisor sum of number " + n + " is " + sum);
-				sumAnnounce.accept(sumReduce);
+				System.out.println("Divisor sum of number " + n + " is " + sumReduce);
 				if (deficiency.test(sumReduce)) return STATE.DEFICIENT;
 				else if (perfection.test(sumReduce)) return STATE.PERFECT;
 				else return STATE.ABUNDANT;
@@ -34,16 +35,14 @@ public class PerfectNumber {
 	
 	public static void detect(int n) {
 		STATE type = process(n);
-		Consumer<STATE> stateAnnounce = (state) -> System.out.println("Number " + n + " is " + state.toString());
-		stateAnnounce.accept(type);
-		System.out.println();
+		System.out.println("Number " + n + " is " + type.toString() + "\n");
 	}
 	
 	public static void main(String[] args) {
 		detect(6);
 		detect(8);
 		detect(20);
-		//do not have the functionality to test 16 with sqrt yet
+		detect(16);
 		detect(1);
 		//tests for divisors for numbers 1, 6 and 8 are already passed in the middle of their previous tests
 	}
